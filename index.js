2ae2acb33ace349a8173b195d04635b7d2337104
Hannah Burdge <hburdge1@gmail.com>
2021-12-05 10:41:27 -0800

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = []
+function writeCards(["name"], occasion) {
+    for (let i = 0; i < "name".length; i++) {
+        cards[i] = `Thank you, ${"name"[i]}, for the wonderful ${occasion} card!`;
+        debugger;
+    }
+
+    return cards
+}
+ 
+function countDown(num) {
+let count = num;
+while (count >= 0) 
+  console.log(count--);
+}
